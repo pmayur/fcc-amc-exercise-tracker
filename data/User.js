264d8842a4089430fd38e0a00b2c692c413392a1
pmayur <thisispatil@gmail.com>
2020-12-03 22:39:43 +0530

    Add User class to store Users

---
+++ data/User.js
@@ -0,0 +1,32 @@
+class User {
+
+    // private variables
+    #id;
+    #username;
+    #exerciseLog = [];
+
+    // constructor
+    constructor(id, username) {
+        this.#id = id;
+        this.#username = username;
+    }
+
+    // GETTERS
+    get id() {
+        return this.#id;
+    }
+
+    get username() {
+        return this.#username;
+    }
+
+    get exerciseCount() {
+        return this.#exerciseLog.length;
+    }
+
+    get exerciseLog() {
+        return this.#exerciseLog;
+    }
+}
+
+module.exports = User;
